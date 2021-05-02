5b4c3f0aa4495301a4c391f442cf0bf49c497342
nadeemali001 <nadeemali001@gmail.com>
2021-05-02 17:36:41 +0530

    Forms_Commit

--- application/routes.py
+++ application/routes.py
@@ -1,21 +1,60 @@
-from application import app
-from flask import render_template
+from application import app, db
+from flask import render_template, request, jsonify
 
+courseData = [{"courseID":"1111","title":"PHP 111","description":"Intro to PHP","credits":"3","term":"Fall, Spring"}, {"courseID":"2222","title":"Java 1","description":"Intro to Java Programming","credits":"4","term":"Spring"}, {"courseID":"3333","title":"Adv PHP 201","description":"Advanced PHP Programming","credits":"3","term":"Fall"}, {"courseID":"4444","title":"Angular 1","description":"Intro to Angular","credits":"3","term":"Fall, Spring"}, {"courseID":"5555","title":"Java 2","description":"Advanced Java Programming","credits":"4","term":"Fall"}]
 
 @app.route('/')
 @app.route('/index')
 @app.route('/home')
 def index():
-    return render_template('index.html', login=False)
+    return render_template('index.html', index=True)
 
 @app.route('/login')
 def login():
-    return render_template('login.html')
+    return render_template('login.html', login=True)
 
 @app.route('/courses')
-def courses():
-    return render_template('courses.html')
+@app.route('/courses/<term>')
+def courses(term="Spring 2019"):
+    return render_template('courses.html', courseData=courseData, courses=True, term=term)
 
 @app.route('/register')
 def register():
-    return render_template('register.html')
+    return render_template('register.html', register=True)
+
+@app.route('/enrollment', methods=['GET','POST'])
+def enrollment():
+    id = request.form.get('courseID')
+    title = request.form.get('title')
+    term = request.form.get('term')
+    data = {
+        'id': id,
+        'title': title,
+        'term': term
+    }
+    return render_template('enrollment.html', enrollment=True, data=data)
+
+@app.route('/api')
+@app.route('/api/')
+@app.route('/api/<idx>')
+def api(idx=None):
+    if(idx == None):
+        return jsonify(courseData)
+    else:
+        return jsonify(courseData[int(idx)])
+
+class User(db.Document):
+    user_id = db.IntField(unique=True)
+    first_name = db.StringField(max_length=50)
+    last_name = db.StringField(max_length=50)
+    email = db.StringField(max_length=30)
+    password = db.StringField(max_length=30)
+
+@app.route('/user')
+def user():
+    #User(user_id=1, first_name="Nadeem", last_name="Ali", email="noddy@inwyt.com", password="abcd1234").save()
+    #User(user_id=2, first_name="Nadir", last_name="Ali", email="nadir@inwyt.com", password="efgh1234").save()
+    #User(user_id=3, first_name="Saba", last_name="Rani", email="saba@inwyt.com", password="ijkl1234").save()
+    users=User.objects.all()
+    return render_template('users.html', user=users)
+
--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
 
-class config(object):
-    SECRET_KY = os.environ.get() or 'dhuih3hu3uid3hih392hirb2i3hd23'
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'dhuih3hu3uid3hih392hirb2i3hd23'
+    MONGODB_SETTINGS = { 'db': 'UTA_Enrollment' }
